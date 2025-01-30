8b42443cee67b79c2ab63684077d8ec5f084db2a
DerKevinRiehl <50080024+DerKevinRiehl@users.noreply.github.com>
2025-01-30 23:33:54 +0100

    trajectory analysis visuals

--- src/SyntheticAnalysis.py
+++ src/SyntheticAnalysis.py
@@ -66,6 +66,96 @@ Q_k_obb = np.diag([1.0, 1.0, 1.0, 1.0, 1.0]) # P_0.copy() # covariance matrix of
 R_k_obb = np.diag([1.0, 1.0, 1.0, 1.0, 1.0]) # P_0.copy() # covariance matrix of error of output
 video_frames_per_second = 25
 
+
+
+
+
+"""
+############## RUN VISUALIZATION
+"""
+veh_trajectory_perf = generateSyntheticTrajectory(noise_pos=0, noise_angle=0, coverage=1)
+veh_trajectory_raw = generateSyntheticTrajectory(noise_pos=0.5, noise_angle=1/360*(2*np.pi), coverage=1)
+first_frame = veh_trajectory_raw["frame_nr"].iloc[0]
+last_frame = veh_trajectory_raw["frame_nr"].iloc[-1] 
+kalman_filtered_trajectory_rts_hbb = calculateKalmanFilteredTrajectory(veh_trajectory_raw, Q_k_hbb, R_k_hbb, first_frame, last_frame, video_frames_per_second, obb=False)
+kalman_filtered_trajectory_rts_obb = calculateKalmanFilteredTrajectory(veh_trajectory_raw, Q_k_obb, R_k_obb, first_frame, last_frame, video_frames_per_second, obb=True)
+kalman_filtered_trajectory_rts_obb = alignTrajectories(kalman_filtered_trajectory_rts_obb, kalman_filtered_trajectory_rts_hbb)
+kalman_filtered_trajectory_rts_hbb = kalman_filtered_trajectory_rts_hbb[["frame_nr", "time", "state1", "state2", "state3", "state4", "state5", "x", "y"]]
+kalman_filtered_trajectory_rts_hbb["vehicle"] = "VEHICLE_1"
+kalman_filtered_trajectory_rts_hbb = kalman_filtered_trajectory_rts_hbb.rename(columns={"x": "x_cartesian", "y": "y_cartesian"})
+processed_hbb = processTrajectory_synthetic(kalman_filtered_trajectory_rts_hbb)
+kalman_filtered_trajectory_rts_obb = kalman_filtered_trajectory_rts_obb[["frame_nr", "time", "state1", "state2", "state3", "state4", "state5", "x", "y"]]
+kalman_filtered_trajectory_rts_obb["vehicle"] = "VEHICLE_1"
+kalman_filtered_trajectory_rts_obb = kalman_filtered_trajectory_rts_obb.rename(columns={"x": "x_cartesian", "y": "y_cartesian"})
+processed_obb = processTrajectory_synthetic(kalman_filtered_trajectory_rts_obb)   
+
+"""
+import pickle
+vehicle_dynamics_path = "../data_benchmark/5_vehicle_information/vehicle_dynamics/"
+with open(vehicle_dynamics_path+"accel_capacity_interpolator.pkl", "rb") as f:
+    accel_max_spl = pickle.load(f)
+with open(vehicle_dynamics_path+"decel_capacity_interpolator.pkl", "rb") as f:
+    decel_min_spl = pickle.load(f)
+reconstr_hbb = reconstruct_trajectories_cvxopt(processed_hbb, accel_max_spl, decel_min_spl)
+reconstr_obb = reconstruct_trajectories_cvxopt(processed_obb, accel_max_spl, decel_min_spl)
+"""
+
+import matplotlib.pyplot as plt
+plt.figure(figsize=(12,6))
+
+plt.subplot(2,3,1)
+plt.title("Trajectory (perfect)")
+plt.plot(veh_trajectory_perf["x"], veh_trajectory_perf["y"])
+
+plt.subplot(2,3,4)
+plt.title("Trajectory (noised)")
+plt.plot(veh_trajectory_raw["x"], veh_trajectory_raw["y"])
+
+plt.subplot(2,3,2)
+plt.title("X-Coordinate")
+plt.plot(veh_trajectory_perf["frame_nr"],                veh_trajectory_perf["x"], label="perfect")
+plt.plot(veh_trajectory_raw["frame_nr"],                 veh_trajectory_raw["x"], label="noised")
+plt.plot(kalman_filtered_trajectory_rts_hbb["frame_nr"], kalman_filtered_trajectory_rts_hbb["state1"], label="kalman HBB")
+plt.plot(kalman_filtered_trajectory_rts_obb["frame_nr"], kalman_filtered_trajectory_rts_obb["state1"], label="kalman OBB")
+# plt.legend()
+plt.xlim(0,200)
+plt.ylim(0,15)
+
+plt.subplot(2,3,5)
+plt.title("Y-Coordinate")
+plt.plot(veh_trajectory_perf["frame_nr"],                veh_trajectory_perf["y"], label="perfect")
+plt.plot(veh_trajectory_raw["frame_nr"],                 veh_trajectory_raw["y"], label="noised")
+plt.plot(kalman_filtered_trajectory_rts_hbb["frame_nr"], kalman_filtered_trajectory_rts_hbb["state2"], label="kalman HBB")
+plt.plot(kalman_filtered_trajectory_rts_obb["frame_nr"], kalman_filtered_trajectory_rts_obb["state2"], label="kalman OBB")
+plt.legend()
+plt.xlim(0,200)
+plt.ylim(26,32)
+
+plt.subplot(2,3,3)
+plt.title("State 4 (V)")
+plt.plot(kalman_filtered_trajectory_rts_hbb["frame_nr"], kalman_filtered_trajectory_rts_hbb["state4"], label="kalman HBB")
+plt.plot(kalman_filtered_trajectory_rts_obb["frame_nr"], kalman_filtered_trajectory_rts_obb["state4"], label="kalman OBB")
+plt.plot(processed_hbb["Frame_ID"], processed_hbb["v_Vel"], label="kalman HBB proc")
+plt.plot(processed_obb["Frame_ID"], processed_obb["v_Vel"], label="kalman OBB proc")
+plt.legend()
+plt.xlim(0,200)
+
+plt.subplot(2,3,6)
+plt.title("State 5 (angle derived)")
+plt.plot(kalman_filtered_trajectory_rts_hbb["frame_nr"], kalman_filtered_trajectory_rts_hbb["state5"], label="kalman HBB")
+plt.plot(kalman_filtered_trajectory_rts_obb["frame_nr"], kalman_filtered_trajectory_rts_obb["state5"], label="kalman OBB")
+plt.plot(processed_hbb["Frame_ID"], processed_hbb["v_AngleVel"], label="kalman HBB proc")
+plt.plot(processed_obb["Frame_ID"], processed_obb["v_AngleVel"], label="kalman OBB proc")
+plt.legend()
+
+plt.tight_layout()
+
+
+
+
+"""
+############## RUN WITHOUT RECONSTRUCTION
+"""
 """
 # Run with Coverage 100%
 coverage=0.5 
@@ -101,8 +191,10 @@ for n in range(0,10):
 
 
 
-
-
+"""
+############## RUN WITH RECONSTRUCTION
+"""
+"""
 # Run with Coverage 100% but Reconstructed Trajectories
 import pickle
 vehicle_dynamics_path = "../data_benchmark/5_vehicle_information/vehicle_dynamics/"
@@ -141,4 +233,4 @@ for n in range(0,10):
         improvement = e_s_avg1-e_s_avg2
         # Print result
         print(coverage, noise_angle, noise_pos, e_s_avg1, e_s_avg2, improvement)
-    
+"""
